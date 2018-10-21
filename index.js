8bd9d3d3c29f294276f4dacf41f3ff1c60c0e82a
Ryan Geary <rtgnj42@gmail.com>
2018-10-21 01:13:31 -0400

    add express static html and abstract port number

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const compression = require('compression');
 
-app.listen(3000, () => {
-  console.log('Server started!');
+const html = __dirname + '/public';
+const port = 3000;
+
+app
+  .use(compression())
+  .use(bodyParser.json())
+  .use(express.static(html))
+
+  .listen(port, () => {
+  console.log('Server started on port ' + port);
 });
+
+app.get('/', function(req, res) {});
 
 app.route('/api/data').get((req, res) => {
   res.send({
