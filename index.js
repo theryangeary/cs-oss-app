c7256553c4db7eade822d1390c957effb0b4c45f
Ryan Geary <rtgnj42@gmail.com>
2018-10-24 11:59:40 -0400

    Add Cors for karma testing of service layer

--- index.js
+++ index.js
@@ -3,22 +3,37 @@ const app = express();
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const data = require('./data.json');
+const cors = require('cors');
 
 const html = __dirname + '/public';
 const port = 3000;
 
-app
-  .use(compression())
-  .use(bodyParser.json())
-  .use(express.static(html))
+var whitelist = [
+    'http://0.0.0.0:3000',
+    'http://0.0.0.0:9876',
+    'http://localhost:9876'
+];
 
-  .listen(port, () => {
-  console.log('Server started on port ' + port);
-});
+var corsOptions = {
+    origin: function(origin, callback) {
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    }
+};
+
+app
+    .use(compression())
+    .use(bodyParser.json())
+    .use(express.static(html))
+    .use(cors(corsOptions))
+
+    .listen(port, () => {
+        console.log('Server started on port ' + port);
+    });
 
 app.get('/', function(req, res) {});
 
 app.route('/api/data').get((req, res) => {
-    console.log("I'm here");
-  res.json(data);
+    console.log("GET /api/data request");
+    res.json(data);
 });
