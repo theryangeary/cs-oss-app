b189a9dfae14c2d73a3b83eba5c46c2dbce03b07
Ryan Geary <rtgnj42@gmail.com>
2018-10-24 02:58:41 -0400

    remove logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,12 +23,15 @@ export class App {
     constructor(private _dataService: DataService) {
         this.items = [];
         this.getItems();
-        this.filteredItems = this.items;
+        // console.log(this.items);
+        // this.filteredItems = this.items;
+        this.filterValue = '';
+        // console.log("Gonna filter" + this.items.length + "values");
+        // this.filter();
         this.title = 'Where Top Domain\s Live';
     }
 
     ngOnInit() {
-        // this.constructMarkers(this.filterValue);
     }
 
     _markerTypeId = MarkerTypeId;
@@ -40,11 +43,13 @@ export class App {
     };
 
     items: GeoItem[];
-    filteredItems: GeoItem[];
+    // filteredItems: GeoItem[];
     selectedItem: GeoItem = null;
-    name = '';
+    // name = '';
     filterValue = '';
     title: string;
+    isLoading: boolean = true;
+    loadIndex: number = 0;
 
     _box: IBox = {
         maxLatitude: 30,
@@ -67,7 +72,7 @@ export class App {
         size: { width: 24, height: 24 }
     };
 
-    _markers: Array<GeoItem> = new Array<GeoItem>();
+    // _markers: Array<GeoItem> = new Array<GeoItem>();
 
     onSelect(item: GeoItem): void {
         if(this.selectedItem != item) {
@@ -78,25 +83,43 @@ export class App {
         console.log("Selected item: " + this.selectedItem);
     }
 
+    getIsLoading() {
+        return this.isLoading;
+    }
+
+    searchMatchesCheckEnd(item: GeoItem, searchq, i) {
+        this.loadIndex = i;
+        if (i === this.items.length - 1) {
+            this.isLoading = false;
+        }
+        this.searchMatches(item, searchq);
+    }
+
     searchMatches(item: GeoItem, searchq) {
         searchq = searchq.toLowerCase();
         return item.domain.toLowerCase().includes(searchq) || searchq === '';
     }
 
     filter() {
-        this.filteredItems.length = 0;
-        this._markers.length = 0;
-        for(let i: number = 0; i<this.items.length; i++) {
-            if (this.searchMatches(this.items[i], this.filterValue)) {
-                this.filteredItems.push(this.items[i]);
-                this._markers.push(this.items[i]);
-            }
-        }
+        this.isLoading = true;
     }
+    // filter() {
+    //     this.filteredItems.length = 0;
+    //     this._markers.length = 0;
+    //     for(let i: number = 0; i<this.items.length; i++) {
+    //         console.log(this.items[i].domain);
+    //         if (this.searchMatches(this.items[i], this.filterValue)) {
+    //             this.filteredItems.push(this.items[i]);
+    //             this._markers.push(this.items[i]);
+    //         }
+    //     }
+    // }
 
     getItems(): void {
         this._dataService.getAllData()
-            .subscribe(items => this.items = items)
+            .subscribe(items => {
+                this.items = items;
+            )
     }
 
 
